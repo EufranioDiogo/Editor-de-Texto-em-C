3fff69d195aa9929a50ddffc115b321ed9d04714
EufranioDiogo <eufraniodiogo5@gmail.com>
2021-05-22 21:35:23 +0100

    TDA for node (line) was created and DoublyLinkedList done

---
+++ tda.h
@@ -0,0 +1,20 @@
+#define TOTAL_CHARACTER_PER_LINE 80
+
+typedef struct node {
+    int idLine;
+    int startCharCollumn;
+    int endCharCollumn;
+    char character[TOTAL_CHARACTER_PER_LINE];
+    int currentLine;
+    struct node *previousLine;
+    struct node *nextLine;
+} Row;
+
+typedef struct list {
+    Row *firstRow;
+    Row *lastRow;
+    int quantRows;
+    int currentLine;
+    int currentCollumn;
+    int flagInsertMode;
+} List;
